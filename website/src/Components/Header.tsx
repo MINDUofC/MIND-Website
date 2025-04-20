78d9b124ae6634425c9c95dc1d1e63b01d46da98
jaivalp22 <jaival.patel@ucalgary.ca>
2025-04-20 02:05:01 -0600

    testing

--- website/src/Components/Header.tsx
+++ website/src/Components/Header.tsx
@@ -11,9 +11,9 @@ const montserrat = Montserrat({
 export function Header() {
     return(
         <main className={montserrat.className}>
-        <header className="animate-slide bg-background h-max p-2 border-b sticky top-0 right-0 z-20 other-bar gap-2 p-2 hover:bg-gray-200 rounded mx-2 hover:underline">
-            <div className="flex h-max justify-between w-fullmax-w-screen-xl mx-auto right-0 px-6 other-bar gap-2 p-2 hover:bg-gray-200 rounded mx-2 hover:underline">
-                <div className="flex items-center gap-3 font-bold other-bar gap-2 p-2 hover:bg-gray-200 rounded mx-2 hover:underline">
+        <header className="animate-slide bg-background h-max p-2 border-b sticky top-0 right-0 z-20 other-bar ">
+            <div className="flex h-max justify-between w-fullmax-w-screen-xl mx-auto right-0 px-6 other-bar ">
+                <div className="flex items-center gap-3 font-bold other-bar ">
                     <img src="/images/black-logo-transparent.png" alt="MIND Logo" className="object-contain w-[75px] sm:w-[35px] md:w-[75px] lg:w-[75px]" />
                     <Link href="/" className="flex justify-center items-center gap-2 ml-0" title="Home">
                         <h1 className=" sm:block text-xl font-bold m-0 mt-1 h-8 ">
@@ -22,7 +22,7 @@ export function Header() {
                     </Link>
                 </div>
 
-                <div className=" font-bold flex ml-auto items-center justify-end items-end flex gap-2 p-2 hover:bg-gray-200 rounded mx-2 hover:underline"> 
+                <div className=" font-bold flex ml-auto items-center justify-end items-end flex "> 
                     {/* gap-6 sm:gap-6 ml-auto md:gap-8 lg:gap-10 items-center text-sm sm:text-base */}
 
                     <NavButton href="/About" label="About" />
--- website/src/Components/NavButton.tsx
+++ website/src/Components/NavButton.tsx
@@ -10,7 +10,7 @@ export function NavButton({
     href,
 }: NavButtonProps) {
     return (
-        <Link href={href} className="flex gap-2 p-2 hover:bg-gray-200 rounded mx-2 hover:underline">
+        <Link href={href} className="flex gap-2 p-2 hover:bg-gray-200 rounded mx-2 ">
             <span>{label}</span>
         </Link>
     );
