2998d0cd6ce14d97635ffa2f36df19bc7fe4d4ba
jaivalp22 <jaival.patel@ucalgary.ca>
2025-04-20 01:56:07 -0600

    testing

--- website/src/Components/Header.tsx
+++ website/src/Components/Header.tsx
@@ -22,7 +22,7 @@ export function Header() {
                     </Link>
                 </div>
 
-                <div className=" font-bold flex ml-auto"> 
+                <div className=" font-bold flex ml-auto items-center"> 
                     {/* gap-6 sm:gap-6 ml-auto md:gap-8 lg:gap-10 items-center text-sm sm:text-base */}
 
                     <NavButton href="/About" label="About" />
