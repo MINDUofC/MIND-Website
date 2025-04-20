0651a63c48be26cc3df3df2480f7fcb478690ff4
jaivalp22 <jaival.patel@ucalgary.ca>
2025-04-20 01:34:33 -0600

    testing

--- website/src/Components/Header.tsx
+++ website/src/Components/Header.tsx
@@ -22,7 +22,7 @@ export function Header() {
                     </Link>
                 </div>
 
-                <div className=" ${myFont.variable} font-bold flex gap-6 sm:gap-6 ml-auto md:gap-8 lg:gap-10 items-center text-sm sm:text-base">
+                <div className=" ${myFont.variable} font-bold flex gap-6 sm:gap-6 ml-auto md:gap-8 lg:gap-10 items-center text-sm sm:text-base right-0">
 
                     <NavButton href="/About" label="About" />
 
