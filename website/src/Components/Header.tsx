daf08c8814e71dd8e6e073922cf393659d707fb2
jaivalp22 <jaival.patel@ucalgary.ca>
2025-04-19 22:57:52 -0600

    changes to the nar-bar for spacing components properly

--- website/src/Components/Header.tsx
+++ website/src/Components/Header.tsx
@@ -12,8 +12,8 @@ export function Header() {
     return(
         <main className={montserrat.className}>
         <header className="animate-slide bg-background h-max p-2 border-b sticky top-0 z-20 other-bar">
-            <div className="flex h-max items-center justify-between w-full other-bar">
-                <div className="flex items-center gap-2 font-bold other-bar">
+            <div className="flex h-max items-center justify-between w-fullmax-w-screen-xl mx-auto px-6 other-bar">
+                <div className="flex items-center gap-3 font-bold other-bar">
                     <img src="/images/black-logo-transparent.png" alt="MIND Logo" className="object-contain w-[75px] sm:w-[35px] md:w-[75px] lg:w-[75px]" />
                     <Link href="/" className="flex justify-center items-center gap-2 ml-0" title="Home">
                         <h1 className=" sm:block text-xl font-bold m-0 mt-1 h-8 ">
@@ -22,7 +22,7 @@ export function Header() {
                     </Link>
                 </div>
 
-                <div className=" ${myFont.variable} font-bold flex gap-4 sm:gap-6 md:gap-8 lg:gap-10 items-center text-sm sm:text-base">
+                <div className=" ${myFont.variable} font-bold flex gap-6 sm:gap-6 md:gap-8 lg:gap-10 items-center text-sm sm:text-base">
 
                     <NavButton href="/About" label="About" />
 
