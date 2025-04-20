1fbc3f5ebf83448dbfad0b88bfc1b5ebf3fa353f
jaivalp22 <jaival.patel@ucalgary.ca>
2025-04-19 21:56:57 -0600

    changes for the nav-bar

--- website/src/Components/NavButton.tsx
+++ website/src/Components/NavButton.tsx
@@ -10,7 +10,7 @@ export function NavButton({
     href,
 }: NavButtonProps) {
     return (
-        <Link href={href} className="flex item-center gap-2 p-2 hover:bg-gray-200 rounded">
+        <Link href={href} className="flex gap-2 p-2 hover:bg-gray-200 rounded">
             <span>{label}</span>
         </Link>
     );
