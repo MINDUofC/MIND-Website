e543c6ee3fe14f1c3174792d8a2cd6f9deb7acb9
Jaival Patel <jaival.patel@ucalgary.ca>
2025-07-24 14:42:39 -0600

    spacing the elemetns evenly

--- website/src/Components/NavButton.tsx
+++ website/src/Components/NavButton.tsx
@@ -10,7 +10,7 @@ export function NavButton({
     href,
 }: NavButtonProps) {
     return (
-        <Link href={href} className=" flex gap-x-6 justify-evenly">
+        <Link href={href} className=" flex gap-x-6 items-center">
             <span>{label}</span>
         </Link>
     );
