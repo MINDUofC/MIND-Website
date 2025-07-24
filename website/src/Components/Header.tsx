2b99244a7bb7459546ba3df8335b95fc6ea722fd
Jaival Patel <jaival.patel@ucalgary.ca>
2025-07-24 15:33:12 -0600

    adding new implementations

--- website/src/Components/Header.tsx
+++ website/src/Components/Header.tsx
@@ -13,7 +13,7 @@ export function Header() {
         <main className={montserrat.className}>
         <header className=" bg-backgroun ">
             <div className=" ">
-                <div className="flex items-center gap-24 font-bold ">
+                <div className="flex items-center font-bold ">
                     <img src="/images/black-logo-transparent.png" alt="MIND Logo" className="object-contain w-[75px] sm:w-[35px] md:w-[75px] lg:w-[75px]" />
                     <Link href="/" className="flex justify-center items-center gap-2 ml-0" title="Home">
                         <h1 className=" sm:block text-xl font-bold m-0 mt-1 h-8 ">
