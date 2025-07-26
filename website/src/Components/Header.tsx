e82a8895e81ff85c6cc5b3dbd37e9c76e7d6ded0
Jaival Patel <jaival.patel@ucalgary.ca>
2025-07-25 21:13:54 -0600

    fixing small navbar issues

--- website/src/Components/Header.tsx
+++ website/src/Components/Header.tsx
@@ -11,16 +11,16 @@ const montserrat = Montserrat({
 export function Header() {
     return(
         <main className={montserrat.className}>
-        <header className=" bg-backgroun ">
+        <header className=" bg-backgroun nav-bar">
             <div className=" justify-between w-full flex-wrap items-center justify-between">
                 <div className="font-bold flex ">
-                    <img src="/images/black-logo-transparent.png" alt="MIND Logo" className="object-contain w-[75px] sm:w-[35px] md:w-[75px] lg:w-[75px]" />
+                    <img src="/images/black-logo-transparent.png" alt="MIND Logo" className="object-contain w-[75px] sm:w-[35px] md:w-[75px] lg:w-[75px] nav-logo" />
                     <Link href="/" className="flex justify-center items-center gap-2 ml-0" title="Home">
                         <h1 className=" sm:block text-xl font-bold m-0 mt-1 h-8 ">
                             MIND
                         </h1>
                     </Link>
-
+                
                     <div className=" sm:gap-6 ml-auto md:gap-8 lg:gap-10 items-center text-sm sm:text-base justify-evenly justify-between items-center flex other-bar"> 
                         <NavButton href="/About" label=" &nbsp; About &nbsp; " />
 
@@ -38,7 +38,7 @@ export function Header() {
 
                     </div>
 
-            </div>
+                </div>
             </div>
         </header>
         </main>
