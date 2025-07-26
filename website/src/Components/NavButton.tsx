d29f76426a2f58b8b38131c06ff1f37509d234ce
Jaival Patel <jaival.patel@ucalgary.ca>
2025-07-25 22:01:30 -0600

    added hover feature for buttons

--- website/src/Components/NavButton.tsx
+++ website/src/Components/NavButton.tsx
@@ -10,7 +10,7 @@ export function NavButton({
     href,
 }: NavButtonProps) {
     return (
-        <Link href={href} className=" ">
+        <Link href={href} className=" hover:bg-gray-200 rounded ">
             <span>{label}</span>
         </Link>
     );
